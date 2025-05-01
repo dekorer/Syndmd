0625dcaf8cc87c840573bb0e720e7bac469cb9de
sujiand99 <158280727+sujiand99@users.noreply.github.com>
2025-05-01 15:34:55 +0900

    Update hwp_writer.py

--- experiments/최수지/hwp_writer.py
+++ experiments/최수지/hwp_writer.py
@@ -1,27 +1,31 @@
 import win32com.client as win32
 
 class HwpWriter:
-    def __init__(self, template_path):
+    def __init__(self):
         self.hwp = win32.gencache.EnsureDispatch("hwpframe.hwpobject")
         self.hwp.XHwpWindows.Item(0).Visible = True
         self.hwp.RegisterModule("FilePathCheckDLL", "FilePathCheckerModule")
-        hwp.Open(r"C:\Users\Suji\Desktop\대표서식1.hwp")
-    
+        self.hwp.Open(r"C:\Users\Suji\Desktop\대표서식1.hwp")  # 고정 템플릿 경로
+
     def write_all(self, parsed_blocks):
         for block in parsed_blocks:
             if block[0] == 'heading':
                 if block[1] == 1:
-                    hwp.PutFieldText("heading1", block[2])
+                    self.hwp.PutFieldText("heading1", block[2])
                 if block[1] == 2:
-                    hwp.PutFieldText("heading2", block[2])
+                    self.hwp.PutFieldText("heading2", block[2])
             elif block[0] == 'list':
                 if block[1] == 1:
-                    hwp.PutFieldText("list1", block[2])
+                    self.hwp.PutFieldText("list1", block[2])
                 if block[1] == 2:
-                    hwp.PutFieldText("list2", block[2])
+                    self.hwp.PutFieldText("list2", block[2])
                 if block[1] == 3:
-                    hwp.PutFieldText("list3", block[2])
+                    self.hwp.PutFieldText("list3", block[2])
                 if block[1] == 4:
-                    hwp.PutFieldText("list4", block[2])
-        hwp.Save()
-        hwp.Quit()
+                    self.hwp.PutFieldText("list4", block[2])
+
+    def save(self, path):
+        self.hwp.SaveAs(path)
+
+    def quit(self):
+        self.hwp.Quit()
